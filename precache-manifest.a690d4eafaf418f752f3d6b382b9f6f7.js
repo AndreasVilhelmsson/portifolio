172036e4164ec17647c17489005380775bbce757
Andreas Vilhelmsson <addevilhelmsson@hotmail.com>
2019-08-30 11:16:35 +0200

    Updates

---
+++ precache-manifest.a690d4eafaf418f752f3d6b382b9f6f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e39f10b91a7b58ae675c41cc80882fd",
+    "url": "/user-rolodex/index.html"
+  },
+  {
+    "revision": "b0148e5a9eba57757b6a",
+    "url": "/user-rolodex/static/css/main.91557f11.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/user-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b0148e5a9eba57757b6a",
+    "url": "/user-rolodex/static/js/main.5a2bd505.chunk.js"
+  },
+  {
+    "revision": "8a75b702f31149523e41",
+    "url": "/user-rolodex/static/js/runtime~main.c864818d.js"
+  }
+]);
